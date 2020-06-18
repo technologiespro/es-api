67356883925601dcbd9a297b959de33c0356312a
technolog <technologies.bro@gmail.com>
2020-06-18 03:23:26 +0300

    /v1/q

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
+const CONFIG = jsonFile.readFileSync('./config.json')
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-    await res.json(true)
+router.post('/v1/q', async function(req, res, next) {
+    let result = null
+    try {
+        result = await axios.get(CONFIG.esApi, req.body)
+    } catch(e) {
+
+    }
+    await res.json(result)
 });
 
 module.exports = router;
