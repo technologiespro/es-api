1a074a8bcafd8eec9634da11a1328765bad61eef
technolog <technologies.bro@gmail.com>
2020-06-18 03:31:26 +0300

    index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ES API for BitShares Blockchain' });
+  res.render('index', { title: 'ES API Wrapper for BitShares Blockchain' });
 });
 
 module.exports = router;
