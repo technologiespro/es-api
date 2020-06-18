9c4d0be5d3d4b85ed08c85db1b63fdb9238bff08
technolog <technologies.bro@gmail.com>
2020-06-18 03:43:38 +0300

    rem cons

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,17 @@ const jsonFile = require('jsonfile')
 const CONFIG = jsonFile.readFileSync('./config.json')
 
 /* GET home page. */
-router.post('/v1/q', async function(req, res, next) {
+router.post('/v1/q', async function (req, res, next) {
     let result = null
     try {
-        result = await axios.get(CONFIG.esApi, req.body)
-    } catch(e) {
+        result = (await axios.get(
+            CONFIG.esApi,
+            req.body, {
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })).data
+    } catch (e) {
 
     }
     await res.json(result)
